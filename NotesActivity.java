93e5936f542708f8416410c45307621a3789eeb6
tuandang <a4dang@ucsd.edu>
2014-02-20 05:00:07 -0800

    Update NotesActivity.java

--- NotesActivity.java
+++ NotesActivity.java
@@ -1,25 +1,93 @@
 package com.example.jobfairapp;
 
+import java.io.ByteArrayOutputStream;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.graphics.drawable.BitmapDrawable;
 
-public class NotesActivity extends Activity {
+
+
+
+public class NotesActivity extends Activity  {
+
+	ImageView imageView;
 	
-	EditText editText;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notes);
+        imageView = (ImageView) findViewById (R.id.imageView1);
+
+    }
+    
+    public void doButton2 (View view)
+    {
+        DBAdapter db = new DBAdapter(this); 
+        db.open();
+
+        if ((db.getImage (1))!=null)
+        {	
+            Log.d ("hererree", "yeaaa0");
+          Bitmap bitmap2 = db.getImage(1);
+          Log.d ("hererree", "yeaaa1");
+          imageView.setImageBitmap (bitmap2);
+        }
+        db.close();
+    }
+    
+    private final int CAMERA_REQUEST_CODE = 1;
+    public void doButton (View view)
+    {
+    	Intent intent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+    	
+    	startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+    
+  
+    
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data)
+    {
+    	// if coming right from the camera
+		if(requestCode == CAMERA_REQUEST_CODE)
+		{
+			// if user keeps the photo
+			if(resultCode == RESULT_OK)
+			{
+				if(data!=null){
+					 DBAdapter db = new DBAdapter(this); 
+
+				        db.open();
+				        ByteArrayOutputStream baos = new ByteArrayOutputStream();  
+				        
+					Bitmap bitmap = data.getParcelableExtra("data");
+
+					bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);   
+			        byte[] photo = baos.toByteArray(); 
+			        Log.d ("CHECKTHIS", "" +photo.length);
+			        db.insertUserDetails("tuan","one", "pass", photo,"visibility");
+					imageView.setImageBitmap(bitmap);
+			        db.close();
+				}
+			}
+			return;
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+    }
+    
 	
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	
-	    // TODO Auto-generated method stub
-	    //setContentView(R.layout.notes);
-	    editText = new EditText(this);
-	    editText.setHint("Add some text here");
-	    setContentView(editText);
 
-	}
+   
 
 }
